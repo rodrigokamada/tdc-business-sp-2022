5148d9e606596b6bed0548a445e4f970062d1759
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-08-18 19:03:28 -0300

    Adding the helmet settings.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import config from 'config';
 import cors from 'cors';
 import express from 'express';
 import * as expressWinston from 'express-winston';
+import helmet from 'helmet';
 import createError from 'http-errors';
 
 import logger from './utils/logger';
@@ -12,6 +13,8 @@ app.use(expressWinston.logger({ winstonInstance: logger }));
 app.use(express.json());
 
 app.use(cors(config.get('cors')));
+
+app.use(helmet(config.get('helmet')));
 
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   next(createError(404));
